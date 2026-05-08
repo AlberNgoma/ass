263b842307a2f14a392bdc5292f332148a868de1
VCach333 <vladmircach333@gmail.com>
2026-05-08 18:30:28 +0100

    config handlebars

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+/* modules import */
 import express from "express";
+import handlebars from 'express-handlebars'
+
+/* config app */
 const app = express();
 
+/* config handlebars */
+const hdb = handlebars.create({
+    
+    defaultLayout: 'main',
+    extname: '.hbs'
+})
+
+app.engine('hbs', hdb.engine)
+app.set('view engine', 'hbs')
+
+/* server listen */
 app.listen(3000, () => {
     console.log("server fly - localhost:3000")
 })
