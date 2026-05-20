06736538140f8cff931e0c61c13188b2f256787b
VCach333 <vladmircach333@gmail.com>
2026-05-20 12:08:25 +0100

    form for sign

---
+++ public/js/views/sign.func.js
@@ -0,0 +1,5 @@
+/* Visibility Toggle between signin and signup */
+function signToggleForm() {
+    document.getElementById('signin').classList.toggle('is-hidden');
+    document.getElementById('signup').classList.toggle('is-hidden');
+}
