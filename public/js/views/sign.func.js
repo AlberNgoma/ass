faabf6688c7d98f6ea57b9243864af3531570192
VCach333 <vladmircach333@gmail.com>
2026-05-20 18:44:49 +0100

    auto visibility sign form

--- public/js/views/sign.func.js
+++ public/js/views/sign.func.js
@@ -3,3 +3,10 @@ function signToggleForm() {
     document.getElementById('signin').classList.toggle('is-hidden');
     document.getElementById('signup').classList.toggle('is-hidden');
 }
+
+/* Auto Visibility Toggle between signin and signup */
+const url = new URLSearchParams(window.location.search)
+
+const urlAction = url.get('action')
+
+if(urlAction == 'up') signToggleForm()
