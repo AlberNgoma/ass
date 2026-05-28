d5f54c0fc51265d1b30d81e3943311349e7c8b5e
VCach333 <vladmircach333@gmail.com>
2026-05-28 13:55:02 +0100

    secretaria dashb. started

--- public/js/views/admin/users.module.js
+++ public/js/views/admin/users.module.js
@@ -2,4 +2,5 @@ function userDeleteSetIdAndRole(id, role) {
 
     const form = document.querySelector('form#user-delete--form')
     form.setAttribute('action', `/admin/users/delete/${id}/${role}`)
+    console.log(`form ${form.id} set action attr. as /admin/users/delete/${id}/${role}`)
 }
